d5910eb57c08958041137b7576d40f0eedaafe4d
Fumiya Kume <fumiya.kume@hotmail.com>
2018-10-24 00:47:12 +0900

    rename

--- meetup/src/main/java/com/meetups/kuxu/meetup/ui/bindingModel/MeetupRowBindingModel.kt
+++ meetup/src/main/java/com/meetups/kuxu/meetup/ui/bindingModel/MeetupRowBindingModel.kt
@@ -1,6 +1,6 @@
 package com.meetups.kuxu.meetup.ui.bindingModel
 
-internal data class meetupRowBindingModel(
+internal data class MeetupRowBindingModel(
   val id: String,
   val meetupTitle: String,
   val meetupUrl: String
