1084daf91b9ca78e2e32bd647f549e128eaefdad
Fumiya Kume <fumiya.kume@hotmail.com>
2018-11-19 18:03:55 +0900

    イベントが選択されるとブラウザに表示されるようなViewModelにした

--- meetup/src/main/java/com/meetups/kuxu/meetup/ui/NearMeetupViewModel.kt
+++ meetup/src/main/java/com/meetups/kuxu/meetup/ui/NearMeetupViewModel.kt
@@ -1,14 +1,26 @@
 package com.meetups.kuxu.meetup.ui
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.meetups.kuxu.meetup.service.ConnpassEventPageViewerService
+import com.meetups.kuxu.meetup.ui.bindingModel.MeetupRowBindingModel
 import com.meetups.kuxu.meetup.ui.bindingModel.MeetupSearchBindingModel
 
 internal class NearMeetupViewModel(
-  nearMeetupListLiveDataFactory: MeetupListLiveDataFactory
-) : ViewModel() {
+  nearMeetupListLiveDataFactory: MeetupListLiveDataFactory,
+  private val connpassEventPageViewerService: ConnpassEventPageViewerService,
+  application: Application
+) : AndroidViewModel(application) {
   var meetupListLiveData = nearMeetupListLiveDataFactory.create()
 
   fun search(bindingModel: MeetupSearchBindingModel) {
     meetupListLiveData.search(bindingModel)
+  }
+
+  fun showEventPage(bindingModel: MeetupRowBindingModel) {
+    if(bindingModel.meetupUrl.isEmpty()){
+      return
+    }
+    connpassEventPageViewerService.showEventPage(bindingModel.meetupUrl)
   }
 }
--- meetup/src/main/java/com/meetups/kuxu/meetup/ui/nearMeetupModule.kt
+++ meetup/src/main/java/com/meetups/kuxu/meetup/ui/nearMeetupModule.kt
@@ -1,9 +1,10 @@
 package com.meetups.kuxu.meetup.ui
 
+import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
 
 internal val nearMeetupModule = module {
   factory { MeetupListLiveDataFactory(get(), get()) }
-  viewModel { NearMeetupViewModel(get()) }
+  viewModel { NearMeetupViewModel(get(), get(), androidApplication()) }
 }
