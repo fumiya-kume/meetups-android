2c2a63a64d6fb802b5970350a327c261375079e7
Fumiya Kume <fumiya.kume@hotmail.com>
2018-12-06 02:15:51 +0900

    コード量を少なくした

--- meetup/src/main/java/com/meetups/kuxu/meetup/domain/repository/MeetupRepositoryImpl.kt
+++ meetup/src/main/java/com/meetups/kuxu/meetup/domain/repository/MeetupRepositoryImpl.kt
@@ -1,8 +1,8 @@
 package com.meetups.kuxu.meetup.domain.repository
 
 import com.meetups.kuxu.connpass_api.MeetupListDataStore
-import com.meetups.kuxu.meetup.entity.LocationEntity
 import com.meetups.kuxu.meetup.entity.MeetupEntity
+import com.meetups.kuxu.meetup.entity.MeetupEntityConverter
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
@@ -14,36 +14,19 @@ internal class MeetupRepositoryImpl(
 
   override fun loadMeetupList(): Deferred<List<MeetupEntity>?> = GlobalScope.async {
     runBlocking {
-      val meetuplist = meetupListDataStore.loadMeetupList().await()
-      meetuplist?.let {
-        it.map {
-          val meetupLocation = LocationEntity(it.lat, it.lon)
-          MeetupEntity(
-            it.id,
-            it.title,
-            it.event_url,
-            0,
-            meetupLocation
-          )
+      meetupListDataStore.loadMeetupList()
+        .await()?.let {
+          MeetupEntityConverter.convert(it)
         }
-      }
     }
   }
 
   override fun searchMeetupList(keyword: String): Deferred<List<MeetupEntity>?> = GlobalScope.async {
     runBlocking {
-      val meetupList = meetupListDataStore.searchMeetupList(keyword).await()
-      meetupList?.let {
-        it.map {
-          MeetupEntity(
-            it.id,
-            it.title,
-            it.event_url,
-            0,
-            LocationEntity(it.lat, it.lon)
-          )
+      meetupListDataStore.searchMeetupList(keyword)
+        .await()?.let {
+          MeetupEntityConverter.convert(it)
         }
-      }
     }
   }
 }
