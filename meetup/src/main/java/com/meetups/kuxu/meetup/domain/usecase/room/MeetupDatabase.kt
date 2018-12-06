6e09db9ab63685bf0c6d434e22d0f89a95e89a85
Fumiya Kume <fumiya.kume@hotmail.com>
2018-12-06 19:27:20 +0900

    Entity の指定がミスっていたのを修正

--- meetup/src/main/java/com/meetups/kuxu/meetup/domain/usecase/room/MeetupDatabase.kt
+++ meetup/src/main/java/com/meetups/kuxu/meetup/domain/usecase/room/MeetupDatabase.kt
@@ -3,7 +3,7 @@ package com.meetups.kuxu.meetup.domain.usecase.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(MeetupDao::class), version = 1)
+@Database(entities = arrayOf(MeetupRoomEntity::class), version = 1)
 abstract class MeetupDatabase : RoomDatabase() {
   abstract fun meetupDao(): MeetupDao
 }
