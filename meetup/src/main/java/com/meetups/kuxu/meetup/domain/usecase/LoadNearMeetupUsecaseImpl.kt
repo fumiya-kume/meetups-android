1c3522b2ac91a467ae034d8b877d18451d090f96
Fumiya Kume <fumiya.kume@hotmail.com>
2018-12-12 18:27:46 +0900

    dao を受け取り、テストしやすくした

--- meetup/src/main/java/com/meetups/kuxu/meetup/domain/usecase/LoadNearMeetupUsecaseImpl.kt
+++ meetup/src/main/java/com/meetups/kuxu/meetup/domain/usecase/LoadNearMeetupUsecaseImpl.kt
@@ -2,9 +2,8 @@ package com.meetups.kuxu.meetup.domain.usecase
 
 import com.meetups.kuxu.meetup.domain.repository.MeetupRepository
 import com.meetups.kuxu.meetup.domain.service.CurrentLocationService
-import com.meetups.kuxu.meetup.domain.usecase.room.MeetupDatabase
+import com.meetups.kuxu.meetup.domain.usecase.room.MeetupDao
 import com.meetups.kuxu.meetup.domain.usecase.room.MeetupRoomEntity
-import com.meetups.kuxu.meetup.entity.LocationEntity
 import com.meetups.kuxu.meetup.entity.MeetupEntity
 import com.meetups.kuxu.meetup.entity.MeetupEntityConverter
 import kotlinx.coroutines.GlobalScope
@@ -15,13 +14,12 @@ import kotlinx.coroutines.runBlocking
 internal class LoadNearMeetupUsecaseImpl(
   private val meetupRepository: MeetupRepository,
   private val currentLocationService: CurrentLocationService,
-  private val meetupDatabase: MeetupDatabase
+  private val meetupDao: MeetupDao
 ) : LoadNearMeetupUsecase {
   override fun execute(): ReceiveChannel<List<MeetupEntity>> = GlobalScope.produce {
     runBlocking {
-
       val readResult =
-        meetupDatabase.meetupDao().readAll()
+        meetupDao.readAll()
           .map { MeetupEntityConverter.convert(it) }
           .map { it.copy(distance = currentLocationService.distanceTo(it.meetupLocation).await()) }
       send(readResult)
@@ -29,7 +27,7 @@ internal class LoadNearMeetupUsecaseImpl(
       meetupRepository.loadMeetupList()
         .await()
         ?.let {
-          meetupDatabase.meetupDao().deleteAll()
+          meetupDao.deleteAll()
           it
             .map {
               MeetupRoomEntity(
@@ -38,12 +36,13 @@ internal class LoadNearMeetupUsecaseImpl(
                 meetupLink = it.meetupLink,
                 lat = it.meetupLocation.lat,
                 lon = it.meetupLocation.lon
-                )
+              )
             }
-            .map { meetupDatabase.meetupDao().insert(it) }
+            .map { meetupDao.insert(it) }
 
-          val resultList = it.map { it.copy(distance = currentLocationService.distanceTo(it.meetupLocation).await()) }
-            .filter { it.distance < 100 }
+          val resultList =
+            it.map { it.copy(distance = currentLocationService.distanceTo(it.meetupLocation).await()) }
+              .filter { it.distance < 100 }
           send(resultList)
         }
     }
