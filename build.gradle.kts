77d913f623176ac46c28058808c3b604c2685abf
Fumiya Kume <fumiya.kume@hotmail.com>
2019-02-04 21:48:29 +0900

    MIgration kts at root Build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.20'
+    val kotlinVersion by extra { "1.3.20" }
     repositories {
         google()
         jcenter()
 
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.3.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -23,6 +24,7 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+
+task("clean", Delete::class) {
+    delete = setOf(rootProject.buildDir)
 }
