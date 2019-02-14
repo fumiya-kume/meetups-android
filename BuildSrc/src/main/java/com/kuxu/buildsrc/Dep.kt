23106c224bac2d475d38f770fec07328fbe92b77
Fumiya Kume <fumiya.kume@hotmail.com>
2019-02-14 16:50:12 +0900

    setting up buildSrc

--- BuildSrc/src/main/java/com/kuxu/buildsrc/Dep.kt
+++ BuildSrc/src/main/java/com/kuxu/buildsrc/Dep.kt
@@ -11,6 +11,7 @@ object Dep {
     object androidx {
         val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
         val kotlinExtension = "androidx.core:core-ktx:1.1.0-alpha04"
+        val appCompat = "androidx.appcompat:appcompat:1.0.0-beta01"
     }
 
     object test {
@@ -20,6 +21,7 @@ object Dep {
     object androidTest {
         object androidx {
             val testRunner = "androidx.test:runner:1.1.0-alpha4"
+            val espresso = "androidx.test.espresso:espresso-core:3.1.0-alpha4"
         }
     }
 }
