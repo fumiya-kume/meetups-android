dc4e1d7d7eb61b9759aaa9c147b5b4bfb39eeaaf
Fumiya Kume <fumiya.kume@hotmail.com>
2019-03-03 13:38:41 +0900

    setup spek

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
         classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0-beta02")
+        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.3.2.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
 
