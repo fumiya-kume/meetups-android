2c85dc1b6a9b2c2b4fb538325f99e7025d5cda91
Fumiya Kume <fumiya.kume@hotmail.com>
2019-03-09 02:20:58 +0900

    検索結果画面の追加と検索画面からの遷移の追加

--- feature/search/src/main/java/com/kuxu/search/SearchMainFragment.kt
+++ feature/search/src/main/java/com/kuxu/search/SearchMainFragment.kt
@@ -5,15 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import com.kuxu.search.databinding.FragmentSearchMainBinding
+import org.koin.android.ext.android.inject
 
 class SearchMainFragment : Fragment() {
+
+    private val navController: NavController by inject()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = FragmentSearchMainBinding.inflate(
             inflater,
             container,
             false
         )
+
+        binding.sarchEventMaterialButton.setOnClickListener {
+            navController.navigate(R.id.action_rootSearchFragment_to_searchResultFragment)
+        }
 
         return binding.root
     }
---
+++ feature/search/src/main/java/com/kuxu/search/SearchResultFragment.kt
@@ -0,0 +1,25 @@
+package com.kuxu.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.kuxu.search.databinding.FragmentSearchResultBinding
+
+class SearchResultFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding =
+            FragmentSearchResultBinding.inflate(
+                inflater,
+                container,
+                false
+            )
+
+        return binding.root
+    }
+}
